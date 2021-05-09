59d275271828b9f96dffa53d2b20166b7c11b0ae
Myil <myilsmp@gmail.com>
2021-05-09 15:28:16 +0530

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div>
+          <h1>About</h1>
+          <p>This is an app to.............</p>
+          <p>version 1.0.0</p>
+        </div>
+    )
+}
